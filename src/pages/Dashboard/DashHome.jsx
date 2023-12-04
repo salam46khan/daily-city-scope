cd370a5c5cd0b9c9975a298643effa45e1329882
salam46khan <salam46.khan@gmail.com>
2023-12-04 21:46:20 +0600

    addd admin home

--- src/pages/Dashboard/DashHome.jsx
+++ src/pages/Dashboard/DashHome.jsx
@@ -1,8 +1,26 @@
+import { Chart } from "react-google-charts";
+import useUser from "../../hook/useUser";
+import useNews from "../../hook/useNews";
+import useAllNews from "../../hook/useAllNews";
 
 const DashHome = () => {
+    const [user] = useUser();
+    const [news] = useNews()
+    const [allnews] = useAllNews()
+    const pending = allnews.length - news.length
     return (
         <div>
-            <p>dash home</p>
+            <h2 className='text-3xl'>Home</h2>
+            <hr />
+            <p className="text-xl">Total User: {user.length}</p>
+            <p className="text-xl">Total Published News: {news.length}</p>
+            <p className="text-xl">Total News: {allnews.length}</p>
+            <p className="text-xl">Total Pending News: {pending}</p>
+            
+
+            <div>
+                
+            </div>
         </div>
     );
 };
